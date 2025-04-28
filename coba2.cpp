5b0ebf543f586c33a31a01723262ff8f2ea5223b
AryaGGWP <arya.lampung.2016@gmail.com>
2025-04-28 19:01:36 +0700

    memanggil class persegi panjang dan objeknya

--- coba2.cpp
+++ coba2.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 class persegiPanjang{
     public:
+    float panjang;
+    float lebar;
+    float luas(){
+        return panjang*lebar;
+    }
 };
+
+int main(){
+    persegiPanjang pp;
+    cout << "Masukkan panjang persegi panjang : "; cin >> pp.panjang;
+    cout << "Masukkan lebar persegi panjang : "; cin >> pp.lebar;
+    cout << "\n-----Hasil Perhitungan-----";
+    cout << "Luas persegi panjang adalah : " << pp.luas() << endl;
+}
